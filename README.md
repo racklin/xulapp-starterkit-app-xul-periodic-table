cb2d2ed8e1a2cc7087d3d1a25b38b6e9fecc1546
Rack Lin <racklin@gmail.com>
2013-01-10 00:41:29 +0800

    XUL Periodic Table for XULApp StarterKit.
    Signed-off-by: Rack Lin <racklin@gmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+XUL Periodic Table For XULApp StarterKit
+-----------------------------
+XUL Periodic Table is XUL Tutorial for XULApp StarterKit.
+
+
+Download pre-build XPI
+-----------------------------
+Download pre-build XPI in [downloads](https://github.com/racklin/xulapp-starterkit-app-xul-periodic-table/tree/master/downloads)
+
+
+Credits
+-----------------------------
+XUL Periodic Table contents is based on (http://www.hevanet.com/acorbin/xul/top.xul).
+
+License
+-----------------------------
+Webapp Wrapper For XULApp StarterKit are licensed under the [MIT License](http://opensource.org/licenses/MIT).
+See LICENSE.TXT for more details.
---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (c) 2012  XULApp StarterKit racklin@gmail.com
+
+CALLDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PACKAGENAME="xulapp-starterkit-xul-periodic-table"
+VERSION="1.0.0"
+
+BUILDID=`date +%Y%m%d`
+DISTDIR="$CALLDIR/dist"
+BUILDDIR="$CALLDIR/build"
+
+echo "Building XUL Periodic Table for XULApp StarterKit"
+
+# Remove build directory
+if [ ! -d "$BUILDDIR" ]; then mkdir -p "$BUILDDIR"; fi
+
+# Copy app directory
+cp -RH "$CALLDIR/chrome" "$BUILDDIR/"
+cp "$CALLDIR/chrome.manifest" "$BUILDDIR/"
+cp "$CALLDIR/install.rdf" "$BUILDDIR/"
+
+# Make sure DISTDIR exists
+if [ ! -d "$DISTDIR" ]; then mkdir -p "$DISTDIR"; fi
+
+cd "$BUILDDIR" && zip -rqX "$DISTDIR/${PACKAGENAME}-${VERSION}.xpi" *
+
+rm -rf $BUILDDIR
